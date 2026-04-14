1964dbee3a75b8d547ba72aa033900ba602c3379
niharikagupta21022006-beep <you@example.com>
2026-04-14 10:05:57 +0530

    dsa

--- Lecture8/Ques5.cpp
+++ Lecture8/Ques5.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
+
+string replaceSpaces(string s){
+    string result = "";
+
+    for(int i = 0;i<s.length();i++){
+        if(s[i] == ' '){
+            result += "@40";
+        }
+
+        else{
+            result += s[i];
+        }
+    }
+
+    return result;
+}
 int main(){
-    
+    string s = "My Name is Niharika";
+    cout << replaceSpaces(s);
+
 }
---
+++ Lecture8/Ques6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "aaabcaabc";
+    string part = "abc";
+
+    while(s.length() != 0 && s.find(part) < s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    cout << s;
+}
